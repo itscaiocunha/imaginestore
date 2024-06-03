f8a5d8ce1bf6d64cbc2f48c82a2b04947b3b5c47
W7 <wi7h.solutions@gmail.com>
2024-06-03 16:43:11 -0300

    feat: update Eduardo

--- w7pay/src/admin/vendas.aspx.cs
+++ w7pay/src/admin/vendas.aspx.cs
@@ -9,6 +9,7 @@ using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Globalization;
 
 namespace w7pay.src
 {
@@ -28,6 +29,27 @@ namespace w7pay.src
                 }
 
                 //atualizacao.GETVendas();
+            }
+        }
+
+        protected void lkbFiltro_Click(object sender, EventArgs e)
+        {
+
+            DateTime datainicio, datafim;
+            if (DateTime.TryParseExact(txtDataInicio.Text, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out datainicio) &&
+                DateTime.TryParseExact(txtDataFim.Text, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out datafim))
+            {
+
+                sdsDados.SelectCommand =
+                    "select f.name, l.name, c.name, m.asset_number, ct.descricao, p.name, coil, quantity, value, occurred_at from vendas v (nolock)" +
+                    "join clientes c on c.id = v.client_id" +
+                    "join locais l on l.id = v.location_id" +
+                    "join maquinas m on m.id = v.machine_id" +
+                    "join produtos p on p.id = v.good_id" +
+                    "join fornecedores f on f.id = p.manufacturer_id" +
+                    "join categorias ct on ct.id = p.category_id" +
+                    "where occurred_at >= '" + datainicio + "' and occurred_at <= '" + datafim + "'" + " order by f.name, l.name, c.name, m.asset_number, ct.descricao, p.name";
+                gdvDados.DataBind();
             }
         }
     }
--- w7pay/src/admin/vendas.aspx.designer.cs
+++ w7pay/src/admin/vendas.aspx.designer.cs
@@ -42,13 +42,31 @@ namespace w7pay.src
         protected global::System.Web.UI.UpdatePanel UpdatePanel1;
 
         /// <summary>
-        /// Controle txtBuscar.
+        /// Controle txtDataInicio.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtBuscar;
+        protected global::System.Web.UI.WebControls.TextBox txtDataInicio;
+
+        /// <summary>
+        /// Controle txtDataFim.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDataFim;
+
+        /// <summary>
+        /// Controle lkbFiltro.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lkbFiltro;
 
         /// <summary>
         /// Controle lblteste.
@@ -58,69 +76,6 @@ namespace w7pay.src
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblteste;
-
-        /// <summary>
-        /// Controle btnImprimir.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnImprimir;
-
-        /// <summary>
-        /// Controle btnDownloadExcel.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnDownloadExcel;
-
-        /// <summary>
-        /// Controle btnDownloadPDf.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnDownloadPDf;
-
-        /// <summary>
-        /// Controle btnDownloadCSV.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnDownloadCSV;
-
-        /// <summary>
-        /// Controle btnView5.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnView5;
-
-        /// <summary>
-        /// Controle btnView20.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnView20;
-
-        /// <summary>
-        /// Controle btnView50.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnView50;
 
         /// <summary>
         /// Controle lblErro.
